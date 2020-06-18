1ddc085686be6500a7e73615d4a66622f5a9e9c0
JustRoxy <roxygamesrus@mail.ru>
2020-06-19 02:02:29 +0300

    err nil wtf moment

--- app/commands.go
+++ app/commands.go
@@ -13,8 +13,5 @@ func (a *App) rollCommand(m *tg.Message) {
 
 func (a *App) vakmanCommand(m *tg.Message) {
 	x := m.Sender.ID % 100
-	_, err := a.bot.Reply(m, fmt.Sprintf("ты вакмен на %v%%", x))
-	if err != nil {
-		fmt.Printf("Пиздячка: %v", m.Sender.ID)
-	}
+	a.bot.Reply(m, fmt.Sprintf("ты вакмен на %v%%", x))
 }
